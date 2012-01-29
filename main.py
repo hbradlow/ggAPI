71017b5b99c0de6b79cf4681317e7ee4c764a171
Kevin Lindkvist <kevinlindkvist@Kevin-Lindkvists-MacBook.local>
2012-01-29 00:09:42 -0800

    dkslfjdskja

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ from crysis2 import *
 from starcraft import * 
 from steamscraper import * 
 
-def getData(name, games=[], id=0, realm="", service=""): 
+def getData(name, games="", id=0, realm=""):
     data = {} 
-    games[0] = games[0].lower() 
+    game = games.lower() 
     service = service.lower() 
-    if games[0] == "wow":
+    if games == "wow":
         scraper = WoWScraper(name=name,realm=realm)
         print scraper.getData()
         return scraper.getData() 
-    elif games[0] == "crysis": 
+    elif games == "crysis": 
         scraper = Crysis2Scraper(name) 
         print scraper.getData()
         return scraper.getData() 
-    elif games[0] == "starcraft": 
+    elif games == "starcraft": 
         scraper = Starcraft2Scraper(name=name, id=id) 
         print scraper.getData() 
         return scraper.getData()
-    elif service == "steam": 
+    elif game == "steam": 
         scraper = SteamScraper(name)
         print scraper.getData() 
         return scraper.getData() 
-    elif service == "xbox": 
+    elif game == "xbox": 
         return data
