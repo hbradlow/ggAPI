9413ecee9b1fbcbcf5b10fef7b0b46a73f2d37db
Will <meaningoflifeis42@gmail.com>
2012-01-28 23:04:13 -0800

    random

--- bf3scraper.py
+++ bf3scraper.py
@@ -3,27 +3,38 @@ from BeautifulSoup import BeautifulSoup
 import urllib
 from scraper import Scraper
 
-def bf3scraper(scraper):
+class bf3scraper(Scraper):
+
     def __init__(self, url):
         page = BeautifulSoup(urllib.urlopen(url))
 
         #general stats
-        rightSideStats = {}
+        rightSideStats = []
         a = page.find('div', {"class":"col_right c21_2"})
-        names = a.findAll('h1')
+        names = a.findAll('h1', text=re.compile('..*'))
         tables = a.findAll('div', {"class":"cont text"})
         for name, table in zip(names, tables):
-            print name
-            print table
             tableKeys = table.findAll('dt', text=re.compile('.*'))
             tableVals = table.findAll('dd', text=re.compile('.*'))
             data = {}
-            for k, v in zip(tableKeys, tableVals):
-                data[k] = v
-            rightSideStats[name] = data
+            i = 0
+            while i<len(tableKeys)-1:
+                data[tableKeys[i]] = tableKeys[i+1]
+                i += 2
+            rightSideStats.append(data)
+
+            while i<len(tableVals)-1:
+                data[tableVals[i]] = tableVals[i+1]
+                i += 2
+            rightSideStats.append(data)
+
+            
         self.rStats = rightSideStats
 
 
 if __name__ == "__main__":
     b = bf3scraper("http://bf3stats.com/stats_pc/JoeHemi")
-    print b.rStats
+    for item in b.rStats:
+        print item
+    
+
