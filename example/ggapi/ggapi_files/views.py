1e215a551bf669371827dec86ac4d7dde91a2a54
Henry Bradlow <henrybrad@gmail.com>
2012-01-28 23:04:22 -0800

    Working on the front end example

--- example/ggapi/ggapi_files/views.py
+++ example/ggapi/ggapi_files/views.py
@@ -17,12 +17,38 @@ from django.contrib.auth.forms import *
 from django.contrib.auth.views import *
 from django.contrib.auth import *
 
+import sys
+import os
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(SITE_ROOT, '../../../'))
+import main
+
 def index(request):
 	if request.method == 'POST':
 		username = request.POST['username']
-		return profile_view(request,username)
+		select = request.POST['select']
+		if select=='starcraft':
+			id = request.POST['id']
+			data = main.getData(username,[select],id=id)
+			return profile_view(request,data,select)
+		if select=='wow':
+			realm = request.POST['realm']
+			data = main.getData(username,[select],realm=realm)
+			return profile_view(request,data,select)
+		if select=='steam':
+			data = main.getData(username,[select])
+			return profile_view(request,data,select)
+		if select=='crysis':
+			data = main.getData(username,[select])
+			return profile_view(request,data,select)
 	return render_to_response("ggapi/index.html",context_instance=RequestContext(request))
 
-def profile_view(request,username):
-	return render_to_response("ggapi/profile_view.html",{"username":username},context_instance=RequestContext(request))
+def profile_view(request,data,game):
+	if game=="starcraft":
+		return render_to_response("ggapi/starcraft_profile_view.html",{"data":data},context_instance=RequestContext(request))
+	if game=="wow":
+		return render_to_response("ggapi/warcraft_profile_view.html",{"data":data},context_instance=RequestContext(request))
+	if game=="crysis":
+		return render_to_response("ggapi/crysis_profile_view.html",{"data":data},context_instance=RequestContext(request))
+	return render_to_response("ggapi/profile_view.html",{"data":data},context_instance=RequestContext(request))
 
